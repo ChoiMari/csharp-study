549bf1baf3389b649dc9d5a274cbc77c8e1a6f9e
ChoiMari <chl98639863@gmail.com>
2025-04-30 13:35:22 +0900

    enum & switch문 실습

---
+++ WindowsFormsApp_05_switch_enum_practice/WindowsFormsApp_05_switch_enum_practice/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApp_05_switch_enum_practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_click = new System.Windows.Forms.Button();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox_input.Location = new System.Drawing.Point(53, 48);
+            this.textBox_input.Multiline = true;
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(172, 36);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(50, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "요일 입력 :";
+            // 
+            // button_click
+            // 
+            this.button_click.Font = new System.Drawing.Font("Gulim", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button_click.Location = new System.Drawing.Point(277, 48);
+            this.button_click.Name = "button_click";
+            this.button_click.Size = new System.Drawing.Size(127, 38);
+            this.button_click.TabIndex = 2;
+            this.button_click.Text = "Click";
+            this.button_click.UseVisualStyleBackColor = true;
+            this.button_click.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(53, 103);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(351, 87);
+            this.textBox_result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 212);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.button_click);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_click;
+        private System.Windows.Forms.TextBox textBox_result;
+    }
+}
+
---
+++ WindowsFormsApp_05_switch_enum_practice/WindowsFormsApp_05_switch_enum_practice/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_05_switch_enum_practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //enum 선언
+        enum Weekday { 
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+            Error = 999
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox_input.Text.Length > 0)
+            {
+                // 사용자에게 받은 입력 값을 input에 저장
+                string input = textBox_input.Text;
+                
+                // enum 객체 생성
+                Weekday weekday;
+                // string타입의 입력값을 enum타입으로 변환함
+                // weekday = (Weekday)Enum.Parse(typeof(Weekday), input);
+                // 변환할 수 없을 경우 예외처리 필요
+                //-> 오류
+
+                switch (input) {
+                    case "월요일":
+                        weekday = Weekday.Monday;
+                        break;
+                    case "화요일":
+                        weekday = Weekday.Tuesday;
+                        break;
+                    case "수요일":
+                        weekday = Weekday.Wednesday;
+                        break;
+                    case "목요일":
+                        weekday = Weekday.Thursday;
+                        break;
+                    case "금요일":
+                        weekday = Weekday.Friday;
+                        break;
+                    case "토요일":
+                        weekday = Weekday.Saturday;
+                        break;
+                    case "일요일":
+                        weekday = Weekday.Sunday;
+                        break;
+                    default:
+                        weekday = Weekday.Error;
+                        textBox_result.Text = "error : 잘못된 입력입니다.\r\n 요일을 입력해주세요.\r\n 입력 예) 일요일";
+                        break; 
+                }
+
+                switch (weekday) {
+
+                    case Weekday.Monday:
+                        textBox_result.Text = "월요일 힘들어요..자고 싶어요..";
+                        break;
+                    case Weekday.Tuesday:
+                        textBox_result.Text = "화요일 힘들어요..자고 싶어요..";
+                        break;
+                    case Weekday.Wednesday:
+                        textBox_result.Text = "수요일 힘들어요..자고 싶어요..";
+                        break;
+                    case Weekday.Thursday:
+                        textBox_result.Text = "목요일 힘들어요..자고 싶어요..";
+                        break;
+                    case Weekday.Friday:
+                        textBox_result.Text = "드디어!! 금요일..";
+                        break;
+                    case Weekday.Saturday:
+                        textBox_result.Text = "토요일 , 집에서 쿨쿨";
+                        break;
+                    case Weekday.Sunday:
+                        textBox_result.Text = "일요일 : 교회에 가요!";
+                        break;
+                    default:
+                        textBox_result.Text = "error : 잘못된 입력입니다.\r\n 요일을 입력해주세요.\r\n 입력 예) 일요일";
+                        break;
+                }
+            }
+            else {  // 사용자 입력이 없는 경우 실행
+                textBox_result.Text = "요일을 입력하십시오.";
+            }
+        }
+    }
+}
